1cfc0761aa5cf4223d338f04b4cdd7a8b397eec9
daniloarantes@gmail.com <>
2025-09-15 19:47:35 -0300

    Inicializado repositório Escola

---
+++ src/main/java/com/mycompany/escola/Escola.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.escola;
+
+/**
+ *
+ * @author danilo.asilv10
+ */
+public class Escola {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
